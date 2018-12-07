4f5d8c8342ae24be6d17fb73297864f17b91d73f
yangshan609 <45680668+yangshan609@users.noreply.github.com>
2018-12-07 19:29:12 +0800

    Update subject.py

--- demo_session/models/subject.py
+++ demo_session/models/subject.py
@@ -8,7 +8,7 @@ class TrainingSubject(models.Model):
 
     name = fields.Char(string='名称')
     person_id = fields.Many2one('res.partner', string='负责人')
-    lesson_ids = fields.One2many('pscloud.training.lesson', 'subject_id', string='课程')
+    lesson_ids = fields.One2many('yangshan609.train.lesson', 'subject_id', string='课程')
     desc = fields.Text(string='描述')
 
 #  vim:et:si:sta:ts=4:sts=4:sw=4:tw=79:
